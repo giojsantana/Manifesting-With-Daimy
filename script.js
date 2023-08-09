009c53f427474801600f24c94daef5033cf3c87f
Giovanni Santana <gjsantana2002@gmail.com>
2023-08-09 10:38:49 -0400

    layout complete

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const cards = [...document.querySelectorAll(".card")];
+const testimonials = [...document.querySelector(".testimonials").children];
+const backBtn = document.querySelector(".back-btn");
+const forwardBtn = document.querySelector(".forward-btn");
 
-cards.forEach((card) => {
-  cardHTML = card.children[0].innerHTML;
-  card.addEventListener("mouseover", function () {
-    card.children[0].classList.add("card-hover");
-    setTimeout(function () {
-      card.children[0].innerHTML = "";
-    }, 200);
+testimonials.forEach((testimonial, i) => {
+  testimonial.style.transform = `translateX(${i * 100}%)`;
+});
+
+let currentSlide = 0;
+const maxSlide = testimonials.length;
+
+backBtn.addEventListener("click", function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1;
+  } else {
+    currentSlide--;
+  }
+  testimonials.forEach((testimonial, i) => {
+    const move = (i - currentSlide) * 100;
+
+    testimonial.style.transform = `translateX(${move}%)`;
   });
-  card.addEventListener("mouseout", function () {
-    card.children[0].classList.remove("card-hover");
-    setTimeout(function () {
-      card.children[0].innerHTML = cardHTML;
-    }, 200);
+});
+
+forwardBtn.addEventListener("click", function () {
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  testimonials.forEach((testimonial, i) => {
+    const move = (i - currentSlide) * 100;
+
+    testimonial.style.transform = `translateX(${move}%)`;
   });
 });
