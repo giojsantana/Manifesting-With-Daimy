a1adde315b14268197787a752a4152a45888117c
Giovanni Santana <gjsantana2002@gmail.com>
2023-08-10 11:05:01 -0400

    testimonial popup

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const testimonials = [...document.querySelector(".testimonials").children];
 const backBtn = document.querySelector(".back-btn");
 const forwardBtn = document.querySelector(".forward-btn");
+const body = document.querySelector("body");
 
 testimonials.forEach((testimonial, i) => {
   testimonial.style.transform = `translateX(${i * 100}%)`;
@@ -32,5 +33,63 @@ forwardBtn.addEventListener("click", function () {
     const move = (i - currentSlide) * 100;
 
     testimonial.style.transform = `translateX(${move}%)`;
+  });
+});
+
+const testimonialBtnMore = [
+  ...document.querySelectorAll(".testimonial-btn--more"),
+];
+const testimonialDiv = document.querySelector(".testimonial-div");
+
+testimonialBtnMore.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    body.style.overflow = "hidden";
+    const testimonial = btn.parentElement.parentElement.parentElement;
+    const html = `
+    <div class="popup-div">
+    
+    <div class="testimonial-popup">
+    <div class="testimonial-img-div testimonial-img--popup">
+    <img
+    class="testimonial-img"
+    src="imgs/${testimonial.dataset.name}.png"
+    alt="Image of ${
+      testimonial.querySelector(".testimonial-author").textContent
+    }"
+    />
+    </div>
+    <h3 class="testimonial-insert--popup">${
+      testimonial.querySelector(".testimonial-insert").textContent
+    }</h3>
+    <p class="testimonial-txt--popup">${testimonial.dataset.text}
+    <div class="testimonial-author testimonial-author--popup">
+    ${testimonial.querySelector(".testimonial-author").textContent}
+    </div>
+    </p>
+    <button class="popup-close-btn"><ion-icon name="caret-back-outline"></ion-icon></button>
+    </div>
+    <div class="blur"></div>
+    </div>
+    `;
+    testimonialDiv.insertAdjacentHTML("afterend", html);
+    const popupDiv = document.querySelector(".popup-div");
+    const testimonialPopup = document.querySelector(".testimonial-popup");
+    const testimonialCloseBtn = document.querySelector(`.popup-close-btn`);
+    const blurDiv = document.querySelector(".blur");
+    popupDiv.style.display = "block";
+
+    setTimeout(function () {
+      console.log("hello");
+      testimonialPopup.classList.add("testimonial-popup-active");
+    }, 10);
+
+    testimonialCloseBtn.addEventListener("click", function () {
+      popupDiv.remove();
+      body.style.overflow = "visible";
+    });
+    blurDiv.addEventListener("click", function () {
+      popupDiv.remove();
+      body.style.overflow = "visible";
+    });
   });
 });
